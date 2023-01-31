91a35ef7a9d1a4451155357a0736d4956ae8aaa7
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-01-31 17:17:47 -0400

    primeira tentativa de mudar a cor de fundo

---
+++ index.js
@@ -0,0 +1,17 @@
+const body = document.querySelector(".body");
+const Btn_Escuro = document.getElementById(".Btn_Escuro");
+
+Btn_Escuro.addEventListener("click", function () {
+    body.style.color = "black"
+})
+
+
+
+
+
+
+
+
+
+
+
